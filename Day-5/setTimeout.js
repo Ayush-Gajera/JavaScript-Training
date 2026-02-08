6616cd4c5f75fd2ef882c568fb88165e4daad081
Ayush-gajera <gajera70964@gmail.com>
2026-02-08 23:46:18 +0530

    Completed Day-5

---
+++ Day-5/setTimeout.js
@@ -0,0 +1,30 @@
+//  Create three functions step1, step2, and step3, each accepting a callback and completing after 1 second using setTimeout.
+// Call them in sequence using nested callbacks.
+// Log "All steps finished" only after step3 completes.
+
+
+
+
+function setp1(callback){
+setTimeout(()=>{
+    callback();
+},1000);
+}
+function step2(callback){
+setTimeout(()=>{
+    callback();
+},1000);
+}
+function step3(callback){
+setTimeout(()=>{
+    callback();
+
+},1000);
+}
+setp1(()=>{
+    step2(()=>{
+        step3(()=>{
+            console.log("All step Completed");
+        })
+    })
+})
