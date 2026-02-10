8ec97f249647428ce60f5619db66e7a644687362
Ayush-gajera <gajera70964@gmail.com>
2026-02-11 00:02:26 +0530

    Add Day-5 DOM and async practice

---
+++ Day-5/promiseWithTimeout.js
@@ -0,0 +1,13 @@
+// Create a function wait(ms) that returns a Promise and resolves after ms milliseconds using setTimeout.
+// Use it to log "2 seconds passed" after 2000 ms.
+
+function wait(ms){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            
+            res(console.log(`${ms} milisecond pass`));
+        },ms);
+    })
+}
+const output =await wait(1000);
+
